be4dfa3e3b91e2144d88067eea65d22bb335997e
Adam Price <komidore64@gmail.com>
2013-01-07 22:39:39 -0500

    rewriting force-save from shell to ruby

--- src/forcesave.rb
+++ src/forcesave.rb
@@ -1,3 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env ruby
 
-screen -p 0 -S minecraft -X stuff $'save-all\n'
+PROGRAM_LOCATION = File.expand_path(File.dirname(__FILE__))
+WORLD_NAME = `#{PROGRAM_LOCATION}/readproperties.rb find level-name`
+
+SAVE_COMMAND = "screen -p 0 -S minecraft -X stuff $'save-all\n'"
+
+print "saving minecraft world..."
+raise RuntimeError, "failed to save the minecraft world [ #{WORLD_NAME} ]" unless system(SAVE_COMMAND)
+puts "DONE"
