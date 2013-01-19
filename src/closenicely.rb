8cf141a9ca5ecda1efd297068cfb9e2790234ee3
Adam Price <komidore64@gmail.com>
2013-01-19 00:57:14 -0500

    converting close-nicely from shell to ruby

--- src/closenicely.rb
+++ src/closenicely.rb
@@ -1,10 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env ruby
+
+$stdout.sync = true
+
+SAVE_MESSAGE = "screen -p 0 -S minecraft -X stuff $'say SERVER GOING DOWN IN 10 SECONDS\n'"
+FORCE_SAVE = "./forcesave.rb 10"
+CLOSE_COMMAND = "screen -p 0 -S minecraft -X stuff $'stop\n'"
+RUNNING_CHECK = "screen -ls | grep minecraft"
+
+running = system(RUNNING_CHECK)
+
+unless running do
+  puts "there is no minecraft server running"
+  exit 1
+end
 
 # notify any logged in players
-( screen -p 0 -S minecraft -X stuff $'say SERVER GOING DOWN IN 10 SECONDS\n' ) && \
+unless system(SAVE_MESSAGE)
+  raise RuntimeError, "unable to send save-message to minecraft server"
+end
 
 # force save the world giving it 10 seconds to complete
-( ./forcesave.rb 10 ) && \
+unless system(FORCE_SAVE)
+  raise RuntimeError, "unable to force-save the minecraft world"
+end
 
 # pass command to close server cleanly
-( screen -p 0 -S minecraft -X stuff $'stop\n ' )
+unless system(CLOSE_COMMAND)
+  raise RuntimeError, "unable to send the stop command to the minecraft server"
+end
