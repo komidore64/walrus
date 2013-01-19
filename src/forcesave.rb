817bd89859c27852ee00d40e4f811296b0ea8b82
Adam Price <komidore64@gmail.com>
2013-01-19 00:56:32 -0500

    fixing the check where forcesave makes sure its argument is a number

--- src/forcesave.rb
+++ src/forcesave.rb
@@ -6,7 +6,7 @@ WORLD_NAME = `#{PROGRAM_LOCATION}/readproperties.rb find level-name`.strip!
 SAVE_COMMAND = "screen -p 0 -S minecraft -X stuff $'save-all\n'"
 
 seconds = ARGV.shift
-time = (seconds.kind_of? Numeric && seconds >= 0) ? seconds : 10
+time = (seconds.to_i > 0) ? seconds : 10 # if argument is not a number or less than zero, just use 10
 
 print "saving [ #{WORLD_NAME} ] to disk ..."
 raise RuntimeError, "failed to save the minecraft world [ #{WORLD_NAME} ]" unless system(SAVE_COMMAND)
